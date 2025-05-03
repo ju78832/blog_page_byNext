325a93dbe4ad3531bb6ba0c6ffca1cd2c1df4e0f
ju78832 <junedgreat89@gmail.com>
2025-05-04 01:08:10 +0530

    Update the change in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,6 +11,28 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
+
+const AceternityLogo = () => {
+  return (
+    <svg
+      width="66"
+      height="65"
+      viewBox="0 0 66 65"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-3 w-3 text-black dark:text-white"
+    >
+      <path
+        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+        stroke="currentColor"
+        strokeWidth="15"
+        strokeMiterlimit="3.86874"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
 
 export default function Navbar() {
   const { data: session, status } = useSession();
@@ -19,7 +41,14 @@ export default function Navbar() {
     <header className="border-b">
       <div className="container flex items-center justify-between h-16">
         <Link href="/" className="font-bold text-lg ml-10">
-          Blogspot
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+          >
+            <AceternityLogo />
+            <span>BlogSpot</span>
+          </HoverBorderGradient>
         </Link>
 
         <nav className="flex items-center gap-4">
